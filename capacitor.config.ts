86bffe3999a44c68dbbbe5bb7428600b08ec3c57
Coucheese <coucheesesas@gmail.com>
2022-08-02 21:45:09 +0200

    Kick Starter

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'choose4me-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
